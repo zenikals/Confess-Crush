bc3df5770545ce8f380c37b698c396303d50dd4a
zenikals <95759408+zenikals@users.noreply.github.com>
2021-12-08 21:09:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "Hellu, bất ngờ chưa",
+    introTitle: 'Nguyễn Mai Phương',
+    introDesc: `Tôi biết bạn đã từng
+    share video này trên insta, và tôi
+    đã thử làm nó một vài lần, bây giờ đã thành công`,
+    btnIntro: '^^Đi thôi^^',
+    title: 'Tôi biết bây giờ bạn đang khá bất ngờ, nhưng đây là phần quà sớm trước noel đi 🥰',
+    desc: 'Hãy tận hưởng nó trong dăm ba giây thôi ',
+    btnYes: 'Yêu mình :>',
+    btnNo: 'Không yêu :<',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại dính phải mình mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì bạn luôn là người khiến mình cười :))) 4 tin nhắn 1 lần cười :v',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
