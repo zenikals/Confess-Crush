97d90a89209b090a61d5da9a3eba5631715a1732
zenikals <95759408+zenikals@users.noreply.github.com>
2022-03-15 00:58:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Hellu, bất ngờ chưa",
-    introTitle: 'Nguyễn Mai Phương',
-    introDesc: `Tôi biết bạn đã từng
-    share video này trên insta, và tôi
-    đã thử làm nó một vài lần, bây giờ đã thành công`,
+    titleWeb: "Basic application",
+    introTitle: 'Cô gái may mắn',
+    introDesc: `Nếu bạn đã từng
+    thấy video này trên MXH, hoặc chưa, thì 
+    tôi có một bất ngờ cho bạn`,
     btnIntro: '^^Đi thôi^^',
-    title: 'Tôi biết bây giờ bạn đang khá bất ngờ, nhưng đây là phần quà sớm trước noel đi 🥰',
-    desc: 'Hãy tận hưởng nó trong dăm ba giây thôi ',
-    btnYes: 'Yêu mình :>',
-    btnNo: 'Không yêu :<',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại dính phải mình mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Vì bạn luôn là người khiến mình cười :))) 4 tin nhắn 1 lần cười :v',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Book GRAB là vậy đấy 🥰',
+    desc: 'thank u ',
+    btnYes: 'Lên xe anh đèo :>',
+    btnNo: 'Không lên xe đâu :<',
+    question: 'Tại sao em lại dí vào link chứ :)))',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Vì trông anh xấu trai vl =)))',
+    mess: 'Anh biết mà =)). ',
+    messDesc: 'Tối nay 7h, anh qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.messenger.com/t/100032643560710' 
 }
